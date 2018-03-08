c0b9416dd456a0326ace955cdc815a4c123adef3
Ken Huh <ken123777@gmail.com>
2018-03-08 14:01:10 +0900

    Initial lecture setup

--- Todo/src/components/App.js
+++ Todo/src/components/App.js
@@ -1,14 +1,38 @@
 import React from 'react'
-import Footer from './Footer'
-import AddTodo from '../containers/AddTodo'
-import VisibleTodoList from '../containers/VisibleTodoList'
+import TodoList from './TodoList'
+import Todo from './Todo';
 
-const App = () => (
-  <div>
-    <AddTodo />
-    <VisibleTodoList />
-    <Footer />
-  </div>
-)
+class App extends React.Component {
+  constructor() {
+    super()
+
+    this.state = {
+      todos: []
+    }
+  }
+
+  onTodoListClick() {
+    console.log('todo list click!')
+  }
+
+  onTodoItemClick() {
+    console.log('todo item click!')
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Vanilla Todo List</h1>
+        <TodoList todos={this.state.todos} onTodoClick={this.onTodoListClick.bind(this)} />
+        {
+          this.state.todos.length &&
+          this.state.todos.map(todo => {
+            return <Todo completed={todo.completed} onClick={this.onTodoItemClick.bind(this)} text={todo.text} />
+          })
+        }
+      </div>
+    )
+  }
+}
 
 export default App
--- Todo/src/index.js
+++ Todo/src/index.js
@@ -1,27 +1,24 @@
 import React from 'react'
 import { render } from 'react-dom'
-import { createStore } from 'redux'
-import { Provider } from 'react-redux'
 import App from './components/App'
-import reducer from './reducers'
-
-const store = createStore(reducer)
 
 render(
-  <Provider store={store}>
-    <App />
-  </Provider>,
+  <App />,
   document.getElementById('root')
 )
 
+// % Key Concepts %
+
 // 1. create store for Todo app
-
 // 2. Usage of react-redux
-
 // 3. Provider
-
 // 4. Container component vs Presentational component
-
 // 5. mapStateToProps
-
 // 6. mapDispatchToProps
+// 7. combineReducers
+
+// % Demo
+
+// 1. Add Todo
+// 2. Show Todo List
+// 3. Filter Todo Item State (completed vs active)
